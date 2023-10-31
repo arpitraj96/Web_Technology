776cf356b07e11515e63f382d66b9c8a26946e3a
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2023-10-31 14:00:37 +0530

    Add files via upload

---
+++ Day11/34_restSpread11.js
@@ -0,0 +1,8 @@
+
+var array1 = [10, 20, 30, 40, 50];
+var array2 = [60, 70, 80, 90, 100];
+var array3 = [1,4,...array1, ...array2];
+console.log(array3);
+let arr = [3, 5, 1];
+
+console.log( Math.max(3, 5, 1) );
